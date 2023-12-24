4e0e86641cdad7a2892c86c6af5c0ae0eba8015f
khasimsharif <115651977+skksharif@users.noreply.github.com>
2023-12-24 15:04:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,10 @@ app.use(cookieParser());
 app.use('/api/users', userRoutes);
 if (process.env.NODE_ENV === 'production') {
     const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    app.use(express.static(path.join(__dirname, '/dist')));
   
     app.get('*', (req, res) =>
-      res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+      res.sendFile(path.resolve(__dirname, '/', 'dist', 'index.html'))
     );
   } else {
     app.get('/', (req, res) => {
